4409ef44c5e91bfcd2ab1be6c29d5f63ab958111
llnnly <llnnnlly@gmail.com>
2024-03-05 20:34:22 +0500

    added simple routing by url

--- src/main/react/auth-service/App.jsx
+++ src/main/react/auth-service/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Recovery from "./pages/Recovery.jsx";
 import RecoveryCode from "./pages/RecoveryCode.jsx";
 import StartPage from "./pages/StartPage.jsx";
@@ -5,9 +7,14 @@ import SignIn from "./pages/SignIn.jsx";
 
 function App() {
   return (
-    <>
-        <Recovery />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<StartPage />} />
+        <Route path="/auth/sign-in" element={<SignIn />} />
+        <Route path="/auth/recovery" element={<Recovery />} />
+        <Route path="/auth/recovery-code" element={<RecoveryCode />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
