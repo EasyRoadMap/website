8c1bc3cd63a6ec9be3f532825ee007cb9e7166a7
Fanatglenta777 <93568829+Fanatglenta777@users.noreply.github.com>
2024-04-24 20:33:44 +0500

    add participant invitation block

--- src/main/react/workspace/components/participants/InviteParticipant.jsx
+++ src/main/react/workspace/components/participants/InviteParticipant.jsx
@@ -1,16 +1,20 @@
 import styles from "./styles.module.css";
-import ParticipantActionsButton from "./ParticipantActionsButton.jsx";
+import Button from "../UI/Button.jsx";
 
 const ParticipantItem = ({ participant }) => {
   return (
     <div className={styles.participantWrapper}>
       <img src={participant.avatar} alt="" className={styles.avatar} />
       <div className={styles.infoWrapper}>
-        <div className={styles.info}>
-          <div className={styles.name}>{participant.name}</div>
+        <div className={styles.infoInvite}>
+          <div className={styles.inviteText}>
+            <div className={styles.name}>{participant.name}</div>
+            <div>•</div>
+            <div>Приглашен(-а)</div>
+          </div>
           <div className={styles.position}>{participant.position}</div>
         </div>
-        <ParticipantActionsButton />
+        <Button text="Отменить" type="outlineSecondary" />
       </div>
     </div>
   );
--- src/main/react/workspace/components/participants/ParticipantsList.jsx
+++ src/main/react/workspace/components/participants/ParticipantsList.jsx
@@ -1,20 +1,20 @@
 import ParticipantItem from "./ParticipantItem.jsx";
+import InviteParticipant from "./InviteParticipant.jsx";
 import styles from "./styles.module.css";
 
-const ParticipantsList = ({
-    participants
-}) => {
-    return (
-        <div className={styles.participantsList}>
-            {participants.map((participant, i) => {
-                return (
-                    <div key={i}>
-                        <ParticipantItem participant={participant} />
-                    </div>
-                );
-            })}
-        </div>
-    );
-}
+const ParticipantsList = ({ participants }) => {
+  return (
+    <div className={styles.participantsList}>
+      {participants.map((participant, i) => {
+        return (
+          <div key={i}>
+            <ParticipantItem participant={participant} />
+          </div>
+        );
+      })}
+      <InviteParticipant participant={participants[0]} />
+    </div>
+  );
+};
 
 export default ParticipantsList;
