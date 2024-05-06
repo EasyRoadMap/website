22b57730d8e476f8bc136d161b2d8ea912e664ec
Kostya Permyakov <mcliyanage2018@gmail.com>
2024-05-07 00:22:50 +0500

    Define certain child endpoints for /workspace instead of '**' wildcard

--- src/main/java/ru/easyroadmap/website/web/workspace/WorkspaceFrontController.java
+++ src/main/java/ru/easyroadmap/website/web/workspace/WorkspaceFrontController.java
@@ -1,11 +1,6 @@
 package ru.easyroadmap.website.web.workspace;
 
-import org.springframework.security.authentication.RememberMeAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,20 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WorkspaceFrontController {
 
     @GetMapping(value = {
-            "/**"
+            "/",
+            "/projects",
+            "/project",
+            "/settings",
+            "/invite"
     })
-    public String handleDefaultRequest(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
-            model.addAttribute("username", userDetails.getUsername());
-
-            if (authentication instanceof RememberMeAuthenticationToken cast) {
-                model.addAttribute("remember_me", "через токен 'запомнить меня'");
-            } else {
-                model.addAttribute("remember_me", "через логин и пароль");
-            }
-        }
-
+    public String handleDefaultRequest() {
         return "workspace";
     }
 
