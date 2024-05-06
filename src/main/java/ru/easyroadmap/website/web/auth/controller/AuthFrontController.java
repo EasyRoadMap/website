b9ebb848c0a743861bb5107b376a6b81b46b0f28
Kostya Permyakov <mcliyanage2018@gmail.com>
2024-05-07 02:36:38 +0500

    Add redirect from /auth to /auth/sign-in

--- src/main/java/ru/easyroadmap/website/web/auth/controller/AuthFrontController.java
+++ src/main/java/ru/easyroadmap/website/web/auth/controller/AuthFrontController.java
@@ -1,12 +1,20 @@
 package ru.easyroadmap.website.web.auth.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/auth")
 public class AuthFrontController {
+
+    @GetMapping("")
+    public void rootEndpointRequest(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/auth/sign-in");
+    }
 
     @GetMapping(value = {
             "/sign-in",
